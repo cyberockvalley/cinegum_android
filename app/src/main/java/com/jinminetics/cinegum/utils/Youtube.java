281ca40ddce8205b1562ff78fbe61bae424bb7f3
cyberockvalley <59527948+cyberockvalley@users.noreply.github.com>
2020-02-02 12:01:03 -0800

    Update Youtube.java

--- app/src/main/java/com/jinminetics/cinegum/utils/Youtube.java
+++ app/src/main/java/com/jinminetics/cinegum/utils/Youtube.java
@@ -1,5 +1,5 @@
 package com.jinminetics.cinegum.utils;
 
 public class Youtube {
-    public static final String API_KEY = "AIzaSyCm1SxeOYmI9R9TUnh1diSc9CYRu80B5yA";
+    public static final String API_KEY = "";
 }
